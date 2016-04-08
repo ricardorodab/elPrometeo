ef3689e41259325fc6f27a196c78a203171d6668
ricardorodab <ricardorodab19@gmail.com>
2016-04-08 01:37:15 -0500

    First commit

---
+++ README.txt
@@ -0,0 +1,11 @@
+Proyecto de ingeniería de software.
+
+El siguiente proyecto es para la clase de ingeniería de software.
+
+
+Integrantes:
+
+García Romo Claudia Fernanda (311153789)
+Jiménez Méndez Ricardo (311085943)
+Rodríguez Abreu José Ricardo (309216139)
+Zamora Gutiérrez Víctor (311529058)
---
+++ lib/DB/03-AlterTable.sql
@@ -0,0 +1,10 @@
+ALTER TABLE agente OWNER TO prometeo;
+ALTER TABLE especialidad OWNER TO prometeo;
+ALTER TABLE mensaje OWNER TO prometeo;
+ALTER TABLE programador OWNER TO prometeo;
+ALTER TABLE usuario OWNER TO prometeo;
+ALTER TABLE pide_servicio OWNER TO prometeo;
+ALTER TABLE presta_servicio OWNER TO prometeo;
+ALTER TABLE registro OWNER TO prometeo;
+ALTER TABLE se_dedica OWNER TO prometeo;
+ALTER TABLE servicio OWNER TO prometeo;
