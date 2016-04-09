257ea9b9d78461770dce3af9cf363ff8addac3ae
ricardorodab <ricardorodab19@gmail.com>
2016-04-09 02:53:44 -0500

    Modificado la posición de las vistas error y éxito, agregado licencia de GNU y agregado comentarios a la clase UsuarioBean. Inicio de sesión exitoso. Creación de scripts para agregar a la base seeds de usuarios, programadores y agentes.

--- src/java/beans/UsuarioBean.java
+++ src/java/beans/UsuarioBean.java
@@ -1,8 +1,30 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+/* -------------------------------------------------------------------
+* UsuarioBean.java
+* versión 1.0
+* Copyright (C) 2016  Kan-Balam.
+* Facultad de Ciencias,
+* Universidad Nacional Autónoma de México, Mexico.
+*
+* Este programa es software libre; se puede redistribuir
+* y/o modificar en los términos establecidos por la
+* Licencia Pública General de GNU tal como fue publicada
+* por la Free Software Foundation en la versión 2 o
+* superior.
+*
+* Este programa es distribuido con la esperanza de que
+* resulte de utilidad, pero SIN GARANTÍA ALGUNA; de hecho
+* sin la garantía implícita de COMERCIALIZACIÓN o
+* ADECUACIÓN PARA PROPÓSITOS PARTICULARES. Véase la
+* Licencia Pública General de GNU para mayores detalles.
+*
+* Con este programa se debe haber recibido una copia de la
+* Licencia Pública General de GNU, de no ser así, visite el
+* siguiente URL:
+* http://www.gnu.org/licenses/gpl.html
+* o escriba a la Free Software Foundation Inc.,
+* 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+* -------------------------------------------------------------------
+*/
 package beans;
 
 import javax.faces.bean.ManagedBean;
@@ -14,76 +36,73 @@ import org.apache.commons.codec.digest.DigestUtils;
 import dao.UsuarioDAO;
 import modelo.Usuario;
 
+/**
+ * @author Jimenez Méndez Ricardo
+ * @version 1.0
+ * @since Mar 09 2016.
+ * <p>
+ * Clase para poder manejar a los usuarios.</p>
+ *
+ * <p>
+ * Clase controladora en particular de las sesiones de los usuarios.</p>
+ */
 @ManagedBean
 @SessionScoped
 public class UsuarioBean {
     
+    /** Es el atributo para poder manejar al usuario actual de nuestra app. */
     private Usuario usuario = new Usuario();
     
+    /**
+     * Metodo que nos regresa al usuario de la clase, el atributo privado de la clase.
+     * @return - El atributo usuario de la clase.
+     */
     public Usuario getUsuario() {
-        
         return usuario;
-        
     }
     
-    public void setUsuario(Usuario usuario) {
-        
-        this.usuario = usuario;
-        
+    /**
+     *
+     * @param usuario
+     */
+    public void setUsuario(Usuario usuario) {        
+        this.usuario = usuario;        
     }
     
-    public String verificarDatos() throws Exception {
-        
+    public String verificarDatos() throws Exception {        
         UsuarioDAO suDAO = new UsuarioDAO();
         Usuario su;
-        String resultado;
-        
-        try {
-            
+        String resultado;        
+        try {            
             su = suDAO.verificarDatos(this.usuario);
             if(su != null) {
-                
+                for (int i = 0; i < 10; i++) {
+                    System.out.println(su.getNombre());
+                }
                 FacesContext.getCurrentInstance().getExternalContext().getSessionMap().put("usuario", su);
-                resultado = "exito";
-               
-            } else {
                 
-                resultado = "error";
-                
+                resultado = "exito";                
+            } else {                
+                resultado = "error";                
             }
-        } catch (Exception e) {
-            
-            throw e;
-            
-        }
-        
-        return resultado;
-        
+        } catch (Exception e) {            
+            throw e;            
+        }        
+        return resultado;        
     }
     
-    public boolean verificarSesion() {
-        
-        boolean estado;
-        
-        if(FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get("usuario") ==  null) {
-            
-            estado = false;
-            
-        } else {
-            
-            estado = true;
-            
-        }
-        
-        return estado;
-        
+    public boolean verificarSesion() {        
+        boolean estado;        
+        if(FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get("usuario") ==  null) {            
+            estado = false;            
+        } else {            
+            estado = true;            
+        }        
+        return estado;        
     }
     
-    public String cerrarSesion() {
-        
+    public String cerrarSesion() {        
         FacesContext.getCurrentInstance().getExternalContext().invalidateSession();
-        return "index";
-        
-    }
-    
-}
+        return "index";        
+    }    
+} //Fin de UsuarioBean.java
--- src/java/dao/UsuarioDAO.java
+++ src/java/dao/UsuarioDAO.java
@@ -1,8 +1,30 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+/* -------------------------------------------------------------------
+* UsuarioDAO.java
+* versión 1.0
+* Copyright (C) 2016  Kan-Balam.
+* Facultad de Ciencias,
+* Universidad Nacional Autónoma de México, Mexico.
+*
+* Este programa es software libre; se puede redistribuir
+* y/o modificar en los términos establecidos por la
+* Licencia Pública General de GNU tal como fue publicada
+* por la Free Software Foundation en la versión 2 o
+* superior.
+*
+* Este programa es distribuido con la esperanza de que
+* resulte de utilidad, pero SIN GARANTÍA ALGUNA; de hecho
+* sin la garantía implícita de COMERCIALIZACIÓN o
+* ADECUACIÓN PARA PROPÓSITOS PARTICULARES. Véase la
+* Licencia Pública General de GNU para mayores detalles.
+*
+* Con este programa se debe haber recibido una copia de la
+* Licencia Pública General de GNU, de no ser así, visite el
+* siguiente URL:
+* http://www.gnu.org/licenses/gpl.html
+* o escriba a la Free Software Foundation Inc.,
+* 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+* -------------------------------------------------------------------
+*/
 package dao;
 
 import org.hibernate.Query;
@@ -26,9 +48,8 @@ public class UsuarioDAO {
         try {
             
             session = HibernateUtil.getSessionFactory().openSession();
-            String sql = "FROM Usuario WHERE nombre = '" + usuario.getNombre() + "' AND contrasenia = '" + usuario.getContrasenia() + "'";
-            Query query = session.createQuery(sql);
-            
+            String sql = "FROM Usuario WHERE correo = '" + usuario.getCorreo() + "' AND contrasenia = '" + usuario.getContrasenia() + "'";
+            Query query = session.createQuery(sql);                          
             if(!query.list().isEmpty()) {
                 
                 su = (Usuario)query.list().get(0);
