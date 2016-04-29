ff20b33cf9b76781908f8be6f5c428c989ca225f
Miguel Angel Piña Avelino <miguel_pinia@ciencias.unam.mx>
2016-04-29 00:58:20 -0500

    Simplica código.

--- src/java/beans/ServicioBean.java
+++ src/java/beans/ServicioBean.java
@@ -5,241 +5,96 @@
 */
 package beans;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
+
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+import dao.OperacionesDAO;
 import modelo.Agente;
-import modelo.Programador;
-import modelo.Registro;
 import modelo.Servicio;
 import modelo.Usuario;
-import org.hibernate.HibernateException;
-import org.hibernate.Query;
-import org.hibernate.Session;
-import org.hibernate.Transaction;
-import util.HibernateUtil;
 
 /**
  *
  * @author ricardo_rodab
  */
+@RequestScoped
 @ManagedBean
 public class ServicioBean {
-    
+
     private Servicio servicio = new Servicio();
-    
+
+    private final OperacionesDAO dao;
+
+    public ServicioBean () {
+        dao = new OperacionesDAO();
+    }
+
     public Servicio getServicio(){
         return this.servicio;
     }
-    
+
     public boolean esElAgente(Usuario usuario){
         /* Primero verificamos que el usuario esté registrado y sea un agente. */
-        Session session;
-        try{
-            session = HibernateUtil.getSessionFactory().getCurrentSession();
-        }catch(HibernateException e){
-            session = null;
-        }
-        if(session == null)
-            session = HibernateUtil.getSessionFactory().openSession();
-        String sql = "FROM Usuario WHERE correo = '" + usuario.getCorreo()
-                + "'";
-        Query query = session.createQuery(sql);
-        // Si no existe el usuario.
-        if (query.list().isEmpty()) {
+        Usuario u = dao.buscaUsuarioPorCorreo(usuario.getCorreo());
+        if (u == null) {
             return false;
         } else {
-            Usuario usuarioNew = (Usuario)query.uniqueResult();
-            if(!usuarioNew.esAgente()){
+            if (!u.esAgente()) {
                 return false;
             }
-            try {
-                //Creamos el servicio nuevo.
-                Transaction tx = session.beginTransaction();
-                Agente dueno = new ArrayList<Agente>(this.servicio.getAgentes()).get(0);
-                session.flush();
-                session.clear();
-                tx.commit();
-                session.close();
-                if(dueno.getIdAgente() == usuario.getAgente().getIdAgente()){
-                    return true;
-                }
-                return false;
-            } catch (Exception e) {
-                return false;
-            }
+            Agente dueno = (Agente) servicio.getAgentes().toArray()[0]; // new ArrayList<Agente>(this.servicio.getAgentes()).get(0);
+            return dueno.getIdAgente() == usuario.getAgente().getIdAgente();
         }
     }
-    
-    public String mostrar(int servicio){
+
+    public String mostrar(int servicio) {
         this.servicio = this.buscar(servicio);
-        if(this.servicio == null)
-            return "error";
-        return "mostrarServicio";
+        return this.servicio == null ? "error" : "mostrarServicio";
     }
-    
+
     /**
      * Método para crear un nuevo servicio.
-     * @param usuarioBean - Es el usuario actual que lo crea.
+     * @param usuario - Es el usuario actual que lo crea.
      * @return Error en caso de que no se pudo crear, servicio en caso contrario.
      */
     public String crear(Usuario usuario){
-        /* Primero verificamos que el usuario esté registrado y sea un agente. */
-        Session session = HibernateUtil.getSessionFactory().openSession();
-        String sql = "FROM Usuario WHERE correo = '" + usuario.getCorreo()
-                + "'";
-        Query query = session.createQuery(sql);
-        // Si no existe el usuario.
-        if (query.list().isEmpty()) {
+        Usuario user = dao.buscaUsuarioPorCorreo(usuario.getCorreo());
+        if (user == null) {
             return "error";
         } else {
-            Usuario usuarioNew = (Usuario)query.uniqueResult();
-            if(!usuarioNew.esAgente())
+            if (!user.esAgente()) {
                 return "error";
-            try {
-                //Creamos el servicio nuevo.
-                Transaction tx = session.beginTransaction();
-                this.servicio.setFinalizado(false);
-                this.servicio.getAgentes().add(usuarioNew.getAgente());
-                usuarioNew.getAgente().getServicios().add(this.servicio);
-                session.update(usuarioNew);
-                session.save(this.servicio);
-                /* La transacción actual. Se utiliza para que persistan los
-                cambios que realicemos en la base */
-                session.flush();
-                session.clear();
-                tx.commit();
-                session.close();
-                return "servicio";
-            } catch (Exception e) {
-                return e.getMessage();
             }
+            servicio.setFinalizado(false);
+            servicio.getAgentes().add(user.getAgente());
+            user.getAgente().getServicios().add(this.servicio);
+            dao.actualizaServicio(user, servicio);
+            return "servicio";
         }
     }
-    
-    public String eliminar(Servicio s){
-        /* Primero verificamos que el usuario no esté registrado */
-        int id = s.getIdServicio();
-        String na = s.getTitulo();
-        System.out.println(id);
-        System.out.println(na);
-        Session session;
-        try{
-            session = HibernateUtil.getSessionFactory().getCurrentSession();
-        }catch(HibernateException e){
-            session = null;
-        }
-        if(session == null)
-            session = HibernateUtil.getSessionFactory().openSession();
-        try {
-            //AQUÍ ME REGRESA QUE SON 0 CUANDO DEBERÍAN SER n > 0
-            /* La transacción actual. Se utiliza para que persistan los
-            cambios que realicemos en la base */
-            Transaction tx = session.beginTransaction();
-            /* Subrutina de eliminación sacada de codejava.net */
-            /* Se convierte el id de usuario a serializable para utilizar el método load */
-            /*
-            Código temporal (agregar ON CASCADE)
-            */
-            //this.servicio = this.buscar(this.servicio.getIdServicio());
-            String sql = "FROM Servicio WHERE idServicio = '" + id
-                    + "'";
-            Query query = session.createQuery(sql);
-            tx.commit();
-            // Si no existe el servicio.
-            if (query.list().isEmpty()) {
-                /*if(session.isOpen())
-                session.close();*/
-                return "error";
-            } else {
-                Servicio servicioNew = (Servicio)query.list().get(0);
-                for (Object o : servicioNew.getAgentes()) {
-                    Agente agente = (Agente)o;
-                    agente.getServicios().remove(servicioNew);
-                    session.merge(agente);
-                }
-                for (Object o : servicioNew.getProgramadors()) {
-                    Programador programador = (Programador)o;
-                    programador.getServicios().remove(servicioNew);
-                    session.merge(programador);
-                }
-                for (Object o : servicioNew.getRegistros()) {
-                    Registro registro = (Registro)o;
-                    registro.setServicio(null);
-                    registro = (Registro)session.merge(registro);
-                    session.delete(registro);
-                }
-                servicioNew.getAgentes().clear();
-                servicioNew.getProgramadors().clear();
-                servicioNew.getRegistros().clear();
-                Servicio snew = (Servicio)session.merge(servicioNew);
-                session.delete(snew);
-                System.out.println("Aquí");
-                /* Se busca la instancia del usuario para eliminarla de la base *
-                Object persistentInstance = session.get(Usuario.class, id);
-                if (persistentInstance != null) {
-                session.delete(persistentInstance);
-                }
-                /* No sé qué tanto de lo que sigue se necesite, pero igual lo
-                dejaré */
-                session.flush();
-                session.clear();
-                tx.commit();
-                //session.close();
-                return "servicio";
-            }
-        } catch (Exception e) {
-            return e.getMessage();
-        }
+
+    public String eliminar(Servicio servicio){
+        dao.eliminarServicio(servicio);
+        return "servicio";
     }
-    
+
     public List<Servicio> getServicios(){
-        Session session;
-        try{
-            session = HibernateUtil.getSessionFactory().getCurrentSession();
-        }catch(HibernateException e){
-            session = null;
-        }
-        if(session == null)
-            session = HibernateUtil.getSessionFactory().openSession();
-        List<Servicio> lista = session.createCriteria(Servicio.class).list();
-        session.close();
-        return lista;
+        return dao.obtenServicios();
     }
-    
+
     public Servicio buscar(int id){
-        /* Primero verificamos que el usuario esté registrado y sea un agente. */
-        Session session;
-        try{
-            session = HibernateUtil.getSessionFactory().getCurrentSession();
-        }catch(HibernateException e){
-            session = null;
-        }
-        if(session == null)
-            session = HibernateUtil.getSessionFactory().openSession();
-        Transaction tx = session.beginTransaction();
-        String sql = "FROM Servicio WHERE idServicio = '" + id
-                + "'";
-        session.flush();
-        session.clear();
-        Query query = session.createQuery(sql);
-        tx.commit();
-        // Si no existe el servicio.
-        if (query.list().isEmpty()) {
-            if(session.isOpen())
-                session.close();
+        Servicio servicio = dao.buscaServicioPorId(id);
+        if (servicio == null) {
             return null;
         } else {
-            Servicio servicioNew = (Servicio)query.list().get(0);
             //Si no hago esto, no se quedan los Sets en el objeto.
-            servicioNew.getAgentes().isEmpty();
-            servicioNew.getRegistros().isEmpty();
-            servicioNew.getProgramadors().isEmpty();
-            if(session.isOpen())
-                session.close();
-            return servicioNew;
+//            servicio.getAgentes().isEmpty();
+//            servicio.getRegistros().isEmpty();
+//            servicio.getProgramadors().isEmpty();
+            return servicio;
         }
     }
-    
+
 }
--- src/java/beans/UsuarioBean.java
+++ src/java/beans/UsuarioBean.java
@@ -1,50 +1,41 @@
 /* -------------------------------------------------------------------
-* UsuarioBean.java
-* versión 1.0
-* Copyright (C) 2016  Kan-Balam.
-* Facultad de Ciencias,
-* Universidad Nacional Autónoma de México, Mexico.
-*
-* Este programa es software libre; se puede redistribuir
-* y/o modificar en los términos establecidos por la
-* Licencia Pública General de GNU tal como fue publicada
-* por la Free Software Foundation en la versión 2 o
-* superior.
-*
-* Este programa es distribuido con la esperanza de que
-* resulte de utilidad, pero SIN GARANTÍA ALGUNA; de hecho
-* sin la garantía implícita de COMERCIALIZACIÓN o
-* ADECUACIÓN PARA PROPÓSITOS PARTICULARES. Véase la
-* Licencia Pública General de GNU para mayores detalles.
-*
-* Con este programa se debe haber recibido una copia de la
-* Licencia Pública General de GNU, de no ser así, visite el
-* siguiente URL:
-* http://www.gnu.org/licenses/gpl.html
-* o escriba a la Free Software Foundation Inc.,
-* 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
-* -------------------------------------------------------------------
+ * UsuarioBean.java
+ * versión 1.0
+ * Copyright (C) 2016  Kan-Balam.
+ * Facultad de Ciencias,
+ * Universidad Nacional Autónoma de México, Mexico.
+ *
+ * Este programa es software libre; se puede redistribuir
+ * y/o modificar en los términos establecidos por la
+ * Licencia Pública General de GNU tal como fue publicada
+ * por la Free Software Foundation en la versión 2 o
+ * superior.
+ *
+ * Este programa es distribuido con la esperanza de que
+ * resulte de utilidad, pero SIN GARANTÍA ALGUNA; de hecho
+ * sin la garantía implícita de COMERCIALIZACIÓN o
+ * ADECUACIÓN PARA PROPÓSITOS PARTICULARES. Véase la
+ * Licencia Pública General de GNU para mayores detalles.
+ *
+ * Con este programa se debe haber recibido una copia de la
+ * Licencia Pública General de GNU, de no ser así, visite el
+ * siguiente URL:
+ * http://www.gnu.org/licenses/gpl.html
+ * o escriba a la Free Software Foundation Inc.,
+ * 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ * -------------------------------------------------------------------
  */
 package beans;
+
+import java.sql.Date;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
 
-import org.apache.commons.codec.digest.DigestUtils;
-
-import dao.UsuarioDAO;
-import java.io.Serializable;
-import java.sql.Date;
-import modelo.Agente;
-import modelo.Programador;
-import modelo.Servicio;
+import dao.OperacionesDAO;
 import modelo.TipoUsuario;
 import modelo.Usuario;
-import org.hibernate.Query;
-import org.hibernate.Session;
-import org.hibernate.Transaction;
-import util.HibernateUtil;
 
 /**
  * @author Jimenez Méndez Ricardo
@@ -54,26 +45,35 @@ import util.HibernateUtil;
  * Clase para poder manejar a los usuarios.</p>
  *
  * <p>
- * Clase controladora en particular de las sesiones de los usuarios.</p>
+ * Clase controladora en particular de las sesiones de los
+ * usuarios.</p>
  */
 @ManagedBean
 @SessionScoped
 public class UsuarioBean {
 
     /**
-     * Es el atributo para poder manejar al usuario actual de nuestra app.
+     * Es el atributo para poder manejar al usuario actual de nuestra
+     * app.
      */
     private Usuario usuario = new Usuario();
 
+    private final OperacionesDAO dao;
+
+    public UsuarioBean() {
+        dao = new OperacionesDAO();
+    }
+
     /**
-     * Metodo que nos regresa al usuario de la clase, el atributo privado de la
-     * clase.
+     * Metodo que nos regresa al usuario de la clase, el atributo
+     * privado de la clase.
      *
      * @return - El atributo usuario de la clase.
      */
     public Usuario getUsuario() {
-        if(usuario.getFechaDeNaciminiento() == null)
+        if (usuario.getFechaDeNaciminiento() == null) {
             usuario.setFechaDeNaciminiento(new Date(1950, 01, 01));
+        }
         return usuario;
     }
 
@@ -84,157 +84,64 @@ public class UsuarioBean {
     public void setUsuario(Usuario usuario) {
         this.usuario = usuario;
     }
-    
-    public String registrar(){       
+
+    public String registrar() {
         return this.registrar(this.usuario.getTipoUsuario());
     }
+
     /**
      * Se registra al usuario en el sistema.
      *
      * @param tipo - el tipo de usuario a registrar.
      */
-    public String registrar(TipoUsuario tipo) {               
-        /* Primero verificamos que el usuario no esté registrado */
-        Session session = HibernateUtil.getSessionFactory().openSession();
-        String sql = "FROM Usuario WHERE correo = '" + usuario.getCorreo()
-                + "'";
-        Query query = session.createQuery(sql);
-        if (!query.list().isEmpty()) {
+    public String registrar(TipoUsuario tipo) {
+        /*
+         * Primero verificamos que el usuario no esté registrado
+         */
+        Usuario u = dao.buscaUsuarioPorCorreo(usuario.getCorreo());
+        if (u == null) {
             return "El usuario con ese correo ya existe.";
         } else {
-            try {
-                /* El usuario no existe por lo que lo registramos */
-                Transaction tx = session.beginTransaction();
-                /* La transacción actual. Se utiliza para que persistan los 
-                    cambios que realicemos en la base */
-                session.save(this.usuario);
-                if (tipo == TipoUsuario.PROGRAMADOR) {
-                    /* Registramos a un programador */
-                    Programador p = new Programador(this.usuario);
-                    /* El programador a registrar en la base */
-                    session.save(p);
-                } else if (tipo == TipoUsuario.AGENTE) {
-                    /* Registramos a un agente */
-                    Agente a = new Agente(this.usuario);
-                    /* El agente a registrar en la base */
-                    session.save(a);
-                }
-                session.flush();
-                session.clear();
-                tx.commit();
-                session.close();                
-                return verificarDatos();
-            } catch (Exception e) {
-                return e.getMessage();
-            }
+            dao.guarda(u, tipo);
+            return verificarDatos();
         }
     }
-    
-    public String irModificar(){
+
+    public String irModificar() {
         return "modificar";
     }
 
     public String modificarPerfil() {
-       /* Primero verificamos que el usuario esté registrado y sea un agente. */
-        Session session = HibernateUtil.getSessionFactory().openSession();
-
-            try {
-                //Creamos el servicio nuevo.
-                Transaction tx = session.beginTransaction();
-                session.update(this.usuario);
-                /* La transacción actual. Se utiliza para que persistan los 
-                    cambios que realicemos en la base */            
-                session.flush();
-                session.clear();
-                tx.commit();
-                session.close();                
-                return "perfil";
-            } catch (Exception e) {
-                return "error";
-            }        
+        boolean actualizado = dao.actualizaUsuario(usuario);
+        return actualizado ? "perfil" : "error";
     }
 
-    public String verificarDatos() throws Exception {
-        UsuarioDAO suDAO = new UsuarioDAO();
-        Usuario su;
-        String resultado;
-        try {
-            su = suDAO.verificarDatos(this.usuario);
-            if (su != null) {
-                this.usuario = su;
-                FacesContext.getCurrentInstance().getExternalContext().getSessionMap().put("usuario", su);
-
-                resultado = "servicio";
-            } else {
-                resultado = "error";
-            }
-        } catch (Exception e) {
-            throw e;
+    public String verificarDatos() {
+        Usuario su = dao.verificarDatos(usuario);
+        if (su != null) {
+            usuario = su;
+            FacesContext.getCurrentInstance().getExternalContext().getSessionMap().put("usuario", su);
+            return "servicio";
         }
-        return resultado;
+        return "error";
     }
 
     public boolean verificarSesion() {
-        boolean estado;
-        if (FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get("usuario") == null) {
-            estado = false;
-        } else {
-            estado = true;
-        }
-        return estado;
+        return FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get("usuario") != null;
     }
 
     public String cerrarSesion() {
         FacesContext.getCurrentInstance().getExternalContext().invalidateSession();
         return "index";
     }
-    
-    
-     /**
+
+    /**
      * Se elimina al usuario del sistema.
      *
-     * @return 
+     * @return
      */
     public String eliminar() {
-        /* Primero verificamos que el usuario no esté registrado */
-        Session session = HibernateUtil.getSessionFactory().openSession();
-        try {
-            /* La transacción actual. Se utiliza para que persistan los 
-                    cambios que realicemos en la base */
-            Transaction tx = session.beginTransaction();
-            /* Subrutina de eliminación sacada de codejava.net */
- /* Se convierte el id de usuario a serializable para utilizar el método load */
-            /*
-            Código temporal (agregar ON CASCADE)
-            */
-            if(this.usuario.esAgente()){
-                Agente ag = this.usuario.getAgente();
-                ag.getServicios().clear();
-                Agente agDel = (Agente)session.merge(ag);
-                session.delete(agDel);
-            }else{
-                Programador prog = this.usuario.getProgramador();
-                prog.getServicios().clear();
-                Programador progDel = (Programador)session.merge(prog);
-                session.delete(progDel);
-            }
-         
-            Serializable id = new Integer(usuario.getIdUsuario());
-            session.delete(this.usuario);
-            /* Se busca la instancia del usuario para eliminarla de la base *
-            Object persistentInstance = session.get(Usuario.class, id);
-            if (persistentInstance != null) {
-                session.delete(persistentInstance);
-            }
-            /* No sé qué tanto de lo que sigue se necesite, pero igual lo 
-                dejaré */
-            session.flush();
-            session.clear();
-            tx.commit();
-            session.close();
-            return "index";
-        } catch (Exception e) {
-            return e.getMessage();
-        }
+        dao.elimina(usuario);
+        return "index";
     }
 } //Fin de UsuarioBean.java
